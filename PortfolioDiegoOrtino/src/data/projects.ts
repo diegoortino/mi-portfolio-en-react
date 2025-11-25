8cfd5af7b51a55c2c3d88a077d7e33e2bb052640
diegoortino <diegoortino@gmail.com>
2025-11-25 18:56:58 -0300

    Sumado proyecto de gestion del gasto

--- PortfolioDiegoOrtino/src/data/projects.ts
+++ PortfolioDiegoOrtino/src/data/projects.ts
@@ -37,4 +37,17 @@ export const projects: Project[] = [
     featured: true,
     date: '2025-09-02',
   },
+  {
+    id: 'gestion-gastos',
+    title: 'App de gestion de gastos personales',
+    description:
+      'App pensada para la gestión de presupuesto mensual y gastos. Utiliza como backEnd tablas de Google Spreadsheets implementada como appWeb mediante Google Appscripts. el FrontEnd está realizado en React. Se utilizaron herramientas como ChatGPT Codex 5.1 Max para la codificacion del proyecto en react.',
+    tech: ['React', 'TypeScript', 'Vite', 'SpreadSheets', 'AppScripts', 'Vercel'],
+    type: 'web',
+    image:
+      'proyects-images/gestionInterna.png',
+    links: { demo: 'https://app-gestion-gastos-diegoortino.vercel.app/login', },
+    featured: true,
+    date: '2024-11-15',
+  }
 ];
